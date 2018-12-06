59f62f62cdee549592a1e0859ed7082eb89c6e7c
Rijel Ek <ekrijel@gmail.com>
2018-12-06 15:33:14 +0200

    added logo

--- client/src/components/Game/header/header.js
+++ client/src/components/Game/header/header.js
@@ -7,8 +7,8 @@ const Header = () =>{
       <header className="header">
       <div className="header__border">
         <Link to="/" className="logo__container">
-          <img src="images/logo-1.png" alt="Logo" className="logo logo--1" />
-          <img src="images/logo-2.png" alt="Logo" className="logo logo--2" />
+          <img src="images/Logo-1.png" alt="Logo" className="logo logo--1" />
+          <img src="images/Logo-2.png" alt="Logo" className="logo logo--2" />
         </Link>
       </div>
     </header>
