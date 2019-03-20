6c78d63c8fe3f1a4db48bbca1379fa4aa0115541
Rijel Ek <ekrijel@gmail.com>
2019-03-20 21:21:42 +0200

    google analytics

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import ReactGA from "react-ga";
 
 import Auth from './hoc/auth';
 
@@ -11,7 +12,7 @@ import Login from './containers/Admin/login';
 import Register from './containers/Admin/register';
 import Logout from './containers/Admin/logout';
 
-// Containders Pages
+// Containers Pages
 import Leaderboard from './containers/Pages/Leaderboard/leaderboard';
 import Credits from './containers/Pages/Credits/credits';
 import Memos from './containers/Pages/Memos/memos';
@@ -20,17 +21,19 @@ import Info from './containers/Pages/Info/info';
 import About from './containers/Pages/About/about';
 import Story from './containers/Pages/Story/Story';
 
-
-
-
-
 //Game
 import Game from './components/Game/game';
 
 
+ReactGA.initialize("UA-136438609-1");
 
 
 class Routes extends Component {
+
+    // componentDidMount(){
+    //     ReactGA.pageview(window.location.pathname + window.location.search);
+    // }
+
     render(){
         return(
 
